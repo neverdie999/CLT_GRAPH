546bb80e3d53985f251b7729a71d9ca3b86abdd3
vinhvo <thanhvinh890@gmail.com>
2018-12-12 10:57:19 +0700

    file-mgmt view changed

--- client/src/modules/sample-message-viewer/file-mgmt/file-mgmt.js
+++ client/src/modules/sample-message-viewer/file-mgmt/file-mgmt.js
@@ -1,7 +1,9 @@
 const ID_FOLDER_OPEN_FILE_MGMT = 'folderOpenFileMgmt'
 const ID_CONTAINER_FILE_MGMT = 'containerFileMgmt'
 const ID_INPUT_SPEC_FILE = 'specFile'
+const ID_INPUT_SPEC_FILE_NAME = 'specFileName'
 const ID_INPUT_SAMPLE_FILE = 'sampleFile'
+const ID_INPUT_SAMPLE_FILE_NAME = 'sampleFileName'
 const ID_BUTTON_VIEW_FULL_TEXT = 'btnViewFullText'
 
 class FileMgmt {
@@ -25,7 +27,11 @@ class FileMgmt {
 
 			const reader = new FileReader();
 			reader.readAsText(file);
-			reader.onload = (event) => {				
+			reader.onload = (event) => {		
+				if ($(`#${ID_INPUT_SPEC_FILE_NAME}`).val() === file.name) return
+
+				$(`#${ID_INPUT_SPEC_FILE_NAME}`).val(file.name)
+
 				this.parent.loadSpecFile(event.target.result)
 			}
 		})
@@ -36,7 +42,11 @@ class FileMgmt {
 
 			const reader = new FileReader();
 			reader.readAsText(file);
-			reader.onload = (event) => {				
+			reader.onload = (event) => {
+				if ($(`#${ID_INPUT_SAMPLE_FILE_NAME}`).val() === file.name) return
+
+				$(`#${ID_INPUT_SAMPLE_FILE_NAME}`).val(file.name)
+
 				this.parent.loadSampleFile(event.target.result)
 			}
 		})
